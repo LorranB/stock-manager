31881b4760fd3183480fb5b6a890e5f822de62ad
Bruno Sanches <lorran.sanches9@gmail.com>
2026-06-09 18:00:31 -0300

    release: versão 6.0 Adicona o icone ao .exe

--- app.py
+++ app.py
@@ -408,26 +408,6 @@ frame_botoes.pack(
 )
 
 
-tk.Button(
-
-    frame_botoes,
-
-    text="Relatório",
-
-    command=lambda: abrir_arquivo(
-        "relatorio_precificacao.txt"
-    ),
-
-    bg="#81C784",
-
-    fg="white"
-
-).grid(
-    row=0,
-    column=0,
-    padx=5
-)
-
 
 tk.Button(
 
